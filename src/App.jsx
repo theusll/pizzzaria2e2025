924689bf491b34f2f6a0dc003b802d72fbdd7ab3
programadormovel <programadormovel@gmail.com>
2025-03-21 09:57:28 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,37 @@ import { useState } from "react"
 
 function App(){
 
-  const [nome, setNome] = useState('Ronaldo')
+
+  const Formulario = ()=> {
+
+    const [nome, setNome] = useState('Ronaldo')
+
+    return (
+      <div>
+        <input 
+          className = "nome"
+          onChange={(e)=>{setNome(e.target.value)}}
+          type="text" />
+
+        <button 
+          className="botao"
+          onClick={()=>{alert(nome)}}      
+        >
+          CLIQUE AQUI
+        </button>
+      </div>
+    )
+  }
+
 
   return (
     <div>
       <h3>Pizzaria 2E</h3>
 
-      <input 
-        className = "nome"
-        onChange={(e)=>{setNome(e.target.value)}}
-        type="text" />
-
-      <button 
-        className="botao"
-        onClick={()=>{alert(nome)}}      
-      >
-        CLIQUE AQUI
-      </button>
+      <Formulario />
+      <Formulario />
+      <Formulario />
+      <Formulario />
 
     </div>
   )
