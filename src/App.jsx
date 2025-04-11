70bd739212371c6539bf51797d1376e850890148
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2025-04-11 14:08:52 +0000

    imagem poke

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import poke from './assets/pokeapi_256.png'
 
 function App(){
 
@@ -43,6 +44,8 @@ function App(){
   return (
     <div>
       <h3>Pizzaria 2E</h3>
+
+      <img src={poke} style={{width:330}} />
 
       <Formulario nome="Ricardo"
           idade={45}
